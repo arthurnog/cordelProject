8417e466c691024fbc06eba22b979d60484d6715
Arthur Nogueira <artuts874@gmail.com>
2026-04-19 19:12:02 -0300

    feat: implementada lógica de movimento do minion

--- scripts/states/minion/minion_idle_state.gd
+++ scripts/states/minion/minion_idle_state.gd
@@ -6,34 +6,30 @@ class_name MinionIdleState
 
 var target_position: Vector2 = Vector2.ZERO
 var direction: Vector2 = Vector2.ZERO
+var attack_distance = 160
+var fliped = false
 
 func start() -> void:
 	calculate_target_position()
-	#body.animator.play("player_walk")
 
 func update(delta: float) -> void:
 	var difference = target_position - body.global_position
 	var distance = difference.length()
 	direction = difference.normalized()
 	var distance_to_player = (body.player.position - body.position).length()
-	var direction_to_player = body.player.position.x - body.global_position.x
 	if distance_to_player <= body.minimum_distance_to_player or distance < 5.0:
+		body.can_attack = !body.can_attack
 		transition.emit(self, "wait")
-	if direction_to_player != 0: #inimigo irá se virar para o player
-		var flip = direction_to_player < 0
-		get_tree().create_timer(body.reaction_time).timeout.connect(_reaction_timer_timeout.bind(flip))
 
 func physics_update(delta: float) -> void:
-	#body.movement.move(direction, delta)
-	pass
+	body.movement.move(direction, delta)
+	body.animator.play("player_walk")
 
 func calculate_target_position():
-	var view_rect = get_viewport().get_visible_rect()
-	target_position = Vector2(
-		randf_range(view_rect.position.x, view_rect.end.x),
-		randf_range(view_rect.position.y, view_rect.end.y)
-	)
-
-func _reaction_timer_timeout(flip) -> void:
-	body.sprite.flip_h = flip
-	body.sprite.position.x = Player.FLIPPED_SPRITE_X_OFFSET * int(flip)
+	if body.can_attack:
+		if body.sprite.flip_h:
+			target_position = body.player.global_position + Vector2(attack_distance,0)
+		else:
+			target_position = body.player.global_position - Vector2(attack_distance,0)
+	else:
+		target_position = body.position - (body.player.position - body.position).normalized()*200
--- scripts/states/minion/minion_wait_state.gd
+++ scripts/states/minion/minion_wait_state.gd
@@ -12,9 +12,22 @@ func start() -> void:
 
 func update(delta: float) -> void:
 	var distance_to_player = (body.position - body.player.position).length()
+
+	var direction_to_player = body.player.position.x - body.global_position.x
+	if direction_to_player != 0: #inimigo irá se virar para o player
+		var flip = direction_to_player < 0
+		get_tree().create_timer(body.reaction_time).timeout.connect(_reaction_timer_timeout.bind(flip))
 	
 	if distance_to_player < body.minimum_distance_to_player:
-		transition.emit(self, "idle") # TODO: Replace with attack state
+		if body.can_attack:
+			transition.emit(self, "idle") # TODO: Replace with attack state
+		else:
+			transition.emit(self, "idle") # Inimigo irá recuar
 
 func timer_end() -> void:
 	transition.emit(self, "idle")
+
+func _reaction_timer_timeout(flip) -> void:
+	#body.can_attack = false
+	body.sprite.flip_h = flip
+	body.sprite.position.x = Player.FLIPPED_SPRITE_X_OFFSET * int(flip)
